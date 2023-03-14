305d774187361ad38b9a2b3217519787458dacb6
fluk3G123 <nitipong4434@gmail.com>
2023-03-14 11:24:42 +0700

    cool

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,49 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *a, int N , int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if( i+1 >= M and (i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N ,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = (double)(rand()%101)/100;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    double sum = 0 ;
+	int index = 0 ;
+   for (int i = 0; i < M*N; i++){
+	if ((i+1)%M == 0 and (i+1) >= M){
+		sum += a[i];
+		b[index]= sum;
+		sum = 0;
+		index++ ;
+	}else {
+		sum += a[i];
+	}
+   }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+	double sum = 0 ;
+	int index = 0 ;
+	for (int i = 0; i < M; i++)
+	{
+		index = i ;
+		for (int j = 0; j < N; j++)
+		{
+			sum += *(a+index);
+			index += M ;	
+		}
+		b[i]=sum;
+		sum = 0;
+	}
+}
